6fc21db62fd739160d3dab8d2f337e2c221f6461
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2026-02-21 09:52:03 +0530

    feat: :sparkles: add api response handler

---
+++ src/types/api/json-api-response.ts
@@ -0,0 +1,13 @@
+export type JsonApiResponse<D = void> =
+	| {
+			success: true;
+			code: number;
+			data?: D;
+			message?: string;
+	  }
+	| {
+			success: false;
+			code: number;
+			message: string;
+			error?: string;
+	  };
